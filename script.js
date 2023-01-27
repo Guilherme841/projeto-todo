3c51c4bb7444c05db6119aec5cd4069d7a8617a5
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-01-26 21:15:10 -0300

    Update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const deletar = document.getElementById('ideletar');
 const adicionar = document.getElementById('iadicionar');
 const concluida = document.getElementById("iconcluida");
 const tarefa = document.getElementById('itarefa');
+const texto = document.getElementById('lista')
 
 deletar.addEventListener('click', deletarTarefa);
 adicionar.addEventListener('click', adicionarTarefa);
@@ -9,12 +10,13 @@ concluida.addEventListener('click', concluirTarefa);
 
 function deletarTarefa () {
     
-}
+};
 
 function adicionarTarefa() {
-    
-}
+    work = String(tarefa.value);
+    texto.innerHTML = work;
+};
 
 function concluirTarefa() {
     
-}
+};
