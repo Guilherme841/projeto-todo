f03337e31e1835adcde7cddee7c7170af651bee0
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-01-27 21:30:51 -0300

    Update

--- script.js
+++ script.js
@@ -8,14 +8,31 @@ deletar.addEventListener('click', deletarTarefa);
 adicionar.addEventListener('click', adicionarTarefa);
 concluida.addEventListener('click', concluirTarefa);
 
+
 function deletarTarefa () {
     
 };
 
 function adicionarTarefa() {
+    work = String(tarefa.value);
+    sel.style.backgroundColor = 'rgba(255, 255, 255, 0.726)'
+    sel.style.padding = '10px 30px'
+    sel.style.borderRadius = '5px'
+    sel.style.cursor = 'pointer'
+    sel.style.textShadow = '1px 1px 1px rgba(0, 0, 0, 0.300)'
+    sel.innerHTML = work;
     
 };
+
+// text-shadow: 1px 1px 1px rgba(0, 0, 0, 0.363);
+// background-color: rgba(255, 255, 255, 0.726);
+// padding: 10px 30px;
+// border-radius: 5px;
+// cursor: pointer;
+
 
 function concluirTarefa() {
     
 };
+
+
