ee30a23e77fbf78a221948cbd684f03210ef75dc
Guilherme Oliveira <116916546+Guilherme841@users.noreply.github.com>
2023-01-29 00:10:03 -0300

    Update

--- script.js
+++ script.js
@@ -2,42 +2,42 @@ const deletar = document.getElementById('ideletar');
 const adicionar = document.getElementById('iadicionar');
 const concluida = document.getElementById("iconcluida");
 const tarefa = document.getElementById('itarefa');
-const sel = document.getElementById('sel')
 
-deletar.addEventListener('click', deletarTarefa);
-adicionar.addEventListener('click', adicionarTarefa);
+deletar.addEventListener('click' , deletarTarefa);
+adicionar.addEventListener('click' , adicionarTarefa);
 concluida.addEventListener('click', concluirTarefa);
-sel.addEventListener('click', seleçao);
 
 function deletarTarefa () {
-    if (sel.classList.contains('selecionou')) {
-        document.getElementById('sel').remove();
-    }
+
 };
 
 function adicionarTarefa() {
-    let work = String(tarefa.value)
-    sel.style.backgroundColor = 'rgba(255, 255, 255, 0.726)'
-    sel.style.padding = '10px 30px'
-    sel.style.borderRadius = '5px'
-    sel.style.cursor = 'pointer'
-    sel.style.textShadow = '1px 1px 1px rgba(0, 0, 0, 0.300)'
-    sel.innerHTML = work
+    if (tarefa.value.length === 0) {
+        alert('Digite sua tarefa!');
+    } else {
+        let item = document.createElement('p');
+        const lista = document.getElementById('lista');
+        const itemDiv = document.querySelector('.item');
+        item.setAttribute('class','item');
+        lista.appendChild(item);
+        let dever = String(tarefa.value);
+        item.style.backgroundColor = 'rgba(255, 255, 255, 0.719)';
+        item.style.padding = '10px 30px';
+        item.style.borderRadius = '5px';
+        item.style.textShadow = '1px 1px 1px rgba(0, 0, 0, 0.438)';
+        item.style.cursor = 'pointer';
+        item.innerHTML = dever;
+        if (> 2) {
+            alert('mais de 6')
+        }
+    };
+
 };
 
 function seleçao() {
-    sel.classList.toggle('selecionou')
-    if (sel.classList.contains('selecionou')) {
-        sel.style.boxShadow = '0 0 0 0.1em #D2B88E'
-        sel.style.transition = '.2s'
-    } else {
-        sel.style.boxShadow = 'none'
-    }
-    
+
 };
 
 function concluirTarefa() {
     
 };
-
-
